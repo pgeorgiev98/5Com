97cb08f7d1858fc62ea414b1f114291f9eed60be
Petko Georgiev <petko.vas.georgiev@gmail.com>
2019-06-19 20:07:55 +0300

    Stop continuous send when the dialog is closed

--- app/continuoussendwindow.cpp
+++ app/continuoussendwindow.cpp
@@ -65,6 +65,7 @@ ContinuousSendWindow::ContinuousSendWindow(SerialPort *port, QWidget *parent)
 	connect(cancelButton, &QPushButton::clicked, this, &ContinuousSendWindow::stopSending);
 	connect(m_sendTimer, &QTimer::timeout, this, &ContinuousSendWindow::sendPacket);
 	connect(m_sendIndefinitely, &QCheckBox::stateChanged, m_packetCount, &QSpinBox::setDisabled);
+	connect(this, &QDialog::finished, this, &ContinuousSendWindow::stopSending);
 }
 
 void ContinuousSendWindow::setInput(const QString &text)
